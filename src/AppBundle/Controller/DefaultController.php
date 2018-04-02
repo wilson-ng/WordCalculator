6eb2fddbe9660f2833d25cf56ad118ab9560d6e9
Wilson Ng <frozalid.wilson@gmail.com>
2018-04-02 22:12:30 +0700

    add scope leftside and rightside from range 0 .. 20 and add converter

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Converter\NumberToWordConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type as FormType;
@@ -22,10 +23,39 @@ class DefaultController extends Controller
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $stringTobeCalculate = $form->getData()['stringCalculation'];
+
+            preg_match('/(.*) (tambah|kali|kurang) (.*)/', $stringTobeCalculate, $arrayTobeCalculate);
+
+            $leftSide = $arrayTobeCalculate[1];
+            $rightSide = $arrayTobeCalculate[3];
+            $operator = $arrayTobeCalculate[2];
+
+            $wordList = array_fill(1, 20, '');
+            foreach ($wordList as $key => &$word) {
+                $word = NumberToWordConverter::handle($key); 
+            }
+            $wordList = array_merge([0 => 'nol'], $wordList);
+
+            $leftSide = $this->convertStringToNumberFromList($wordList, $leftSide); 
+            $rightSide = $this->convertStringToNumberFromList($wordList, $rightSide); 
+
+            if ($leftSide > 20 || $leftSide < 0 || $rightSide > 20 || $rightSide < 0) {
+                $this->addFlash('notice', 'Nilai valid sisi kiri dan kanan adalah rentang 0 sampai dengan 20.');
+                return $this->redirectToRoute('homepage');
+            }
+
         }
 
         return $this->render('default/index.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    private function convertStringToNumberFromList(array $list, String $word): int
+    {
+        if (!in_array($word, $list)) {
+            return -1;
+        }
+        return array_search($word, $list);
     }
 }
---
+++ src/AppBundle/Converter/NumberToWordConverter.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\Converter;
+
+class NumberToWordConverter
+{
+    public static function handle(int $number): String 
+    {
+        return rtrim(ltrim(self::convert($number)));
+    }
+
+    public static function convert(int $number): String 
+    {
+        $wordNumber = ['', 'satu', 'dua', 'tiga', 'empat', 'lima', 'enam', 'tujuh', 'delapan', 'sembilan', 'sepuluh', 'sebelas'];
+
+        if ($number >=  0 && $number < 12) {
+            return ' ' . $wordNumber[$number];
+        } elseif ($number < 20) {
+            return self::convert($number - 10) . ' belas';
+        } elseif ($number < 100) {
+            return self::convert((int) floor($number / 10)) . ' puluh' . self::convert($number % 10);
+        } elseif ($number < 200) { 
+            return 'seratus ' . self::convert($number - 100);
+        } elseif ($number < 1000) { 
+            return self::convert((int) floor($number / 100)) . ' ratus' . self::convert($number % 100);
+        }
+    } 
+}
