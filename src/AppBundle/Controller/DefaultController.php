9ed5188c4966926b947559829033c5ac83bad4d6
Wilson Ng <frozalid.wilson@gmail.com>
2018-04-02 21:12:19 +0700

    add form for retrive string tobe calculate from frontend

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type as FormType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,9 +14,18 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        $form = $this->createFormBuilder()
+                     ->add('stringCalculation', FormType\TextType::class)
+                     ->add('submit', FormType\SubmitType::class)
+                     ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $stringTobeCalculate = $form->getData()['stringCalculation'];
+        }
+
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'form' => $form->createView(),
         ]);
     }
 }
